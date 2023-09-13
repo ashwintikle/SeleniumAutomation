d89e591a74daa1728d3d2f70d1362bdd0b1ba52e
ashwintikle <ashwin210590@gmail.com>
2023-09-14 00:31:56 +0530

    Commit from user 1

---
+++ src/test/java/PracticeGit/APITesting.java
@@ -0,0 +1,12 @@
+package PracticeGit;
+
+import org.testng.annotations.Test;
+
+public class APITesting {
+	
+	@Test
+	public void testAPI() {
+		System.out.println("this is api testing code line 1");
+	}
+
+}
---
+++ src/test/java/PracticeGit/MobileTesting.java
@@ -0,0 +1,11 @@
+package PracticeGit;
+
+import org.testng.annotations.Test;
+
+public class MobileTesting {
+
+	@Test
+	public void testMobileApp() {
+		System.out.println("this is mobile app testing code line 1");
+	}
+}
